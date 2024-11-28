d46e6ae2079201ad693da8465916c1a37f16dd74
dmswl2515 <dmswl2515@gmail.com>
2024-11-28 19:20:41 +0900

    first commit

---
+++ src/main/java/com/cardfolio/springboot/CardFolioApplication.java
@@ -0,0 +1,13 @@
+package com.cardfolio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardFolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardfolio/springboot/CardFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardfolio.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardFolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
