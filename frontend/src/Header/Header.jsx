3047bf7e85ec6fa665d7878e93b2b81e96040cb3
dmswl2515 <dmswl2515@gmail.com>
2024-12-31 19:30:21 +0900

    add cardFolio logo

--- frontend/src/Header/Header.jsx
+++ frontend/src/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css"
+import logo from '../Assets/CardFolio_logo.png';
 
 const Header = () => {
     return (
@@ -7,13 +8,22 @@ const Header = () => {
                 {/* 상단 정보 영역 */}
                 <div className="header-top">
                     <div className="header-top-left">
-                        <span className="rank">3</span>
-                        <img src="samsung-card-image-url" alt="카드이미지" className="card-image"/>
-                        <span className="card-info">삼성카드 & MILEAGE PLATINUM (스카이패스)</span>
+                        <span className="rank">
+                            <span className="rank-number">1</span>
+                            <img src="https://www.shinhancard.com/pconts/images/contents/card/plate/cdCreditAUAARH.png" alt="카드이미지" className="card-image"/>
+                            <span className="card-info">신한카드 Mr.Life </span>
+                            <i className="fas fa-caret-down"></i>
+                        </span>
                     </div>
+
+                    <div className="header-top-center">
+                        <img src={logo} alt="Logo" className="logo-image"/>
+                    </div>
+
                     <div className="header-top-right">
                         <span>서두르세요! 현대카드M</span>
                         <span className="event-text">최대 15만원 캐시백 제공 이벤트</span>
+                        <img src="https://www.hyundaicard.com/img/com/card/card_ME4_BA_f.png" alt="카드이미지" className="card-image2"/>
                     </div>
                 </div>
 
