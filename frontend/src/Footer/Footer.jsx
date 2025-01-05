9f5e1d1d0d8d61914393fdbb6b67b18c77adf743
dmswl2515 <dmswl2515@gmail.com>
2025-01-05 19:30:37 +0900

    add footer's bottom

--- frontend/src/Footer/Footer.jsx
+++ frontend/src/Footer/Footer.jsx
@@ -32,13 +32,49 @@ const Footer = () => {
                     </p>
                 </div>
             </div>
+            
+            {/* Footer Links */}
+            <div className="footer-links">
+                <div className="footer-social">
+                    <a href="#" target="_blank">
+                        <i class="fab fa-youtube"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fa-solid fa-n"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
 
+                <div className="footer-nav">
+                <a href="#">ABOUT</a>
+                <a href="#">SERVICE</a>
+                <a href="#">CONTACT</a>
+                <a href="#">CARDGORILLA</a>
+                <a href="#">PRESTIGEGORILLA</a>
+                <a href="#">GOSTY</a>
+                </div>
+            </div>
 
+            {/* Footer Bottom */}
+            <div className="footer-bottom">
+                <div className="horizontal-container">
+                    <p>(주)카드폴리오디스트릭트</p> 
+                    <p>대표이사 : 김은지</p> 
+                    <p>사업자등록번호 : 304-58-10583</p> 
+                    <p>서울특별시 노원구 노원로 532 카드폴리오</p> 
+                    <p>광고/제휴문의 :{" "}
+                        <a href="mailto:dmswl2515@gmail.com">
+                            dmswl2515@gmail.com
+                        </a>{" "}</p>
+                    <p>고객문의 : <a href>바로가기 &gt;</a></p>
+                </div>
+                <p>© 2024-2025 CardFolio All Rights Reserved.</p>
+            </div>
         </footer>
     );
 }
-
-
 
 
 export default Footer;
