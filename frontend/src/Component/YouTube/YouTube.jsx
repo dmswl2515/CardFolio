b4b4cf9ae83ababff7aa6f3c8369f22b619db2f4
dmswl2515 <dmswl2515@gmail.com>
2025-01-12 18:59:43 +0900

    short 영상 섹션 정렬 수정

--- frontend/src/Component/YouTube/YouTube.jsx
+++ frontend/src/Component/YouTube/YouTube.jsx
@@ -48,7 +48,7 @@ const YouTube = () => {
 
     return (
         <div className="youtube">
-            <h1>Shorts 영상</h1>
+            <h2 className="shorts-title">Shorts 영상</h2>
             <div className="shorts-container">
                 {videos.map((video) => (
                 <div
