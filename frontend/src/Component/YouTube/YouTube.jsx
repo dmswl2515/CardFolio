8fb9619f405051e715a10fa987a43be202485021
dmswl2515 <dmswl2515@gmail.com>
2025-01-13 00:16:52 +0900

    iframe css 수정 및 youtube url embeded 형식으로 수정

--- frontend/src/Component/YouTube/YouTube.jsx
+++ frontend/src/Component/YouTube/YouTube.jsx
@@ -17,31 +17,31 @@ const YouTube = () => {
         {
             id: 1,
             thumbnail: "https://img.youtube.com/vi/h7atcs15IxM/hqdefault.jpg",
-            url: "https://youtube.com/shorts/h7atcs15IxM?si=d9DAE780eqqkqujw",
+            url: "https://www.youtube.com/embed/h7atcs15IxM",
             title: "Video 1",
         },
         {
             id: 2,
             thumbnail: "https://img.youtube.com/vi/gS9PHbAL290/hqdefault.jpg",
-            url: "https://youtube.com/shorts/gS9PHbAL290?si=mviJtWw23cGCpCum",
+            url: "https://www.youtube.com/embed/gS9PHbAL290",
             title: "Video 2",
         },
         {
             id: 3,
             thumbnail: "https://img.youtube.com/vi/uJhcXMUln8I/hqdefault.jpg",
-            url: "https://youtube.com/shorts/uJhcXMUln8I?si=-E96NL2sS6KgIGPq",
+            url: "https://www.youtube.com/embed/uJhcXMUln8I",
             title: "Video 3",
         },
         {
             id: 4,
             thumbnail: "https://img.youtube.com/vi/N_JtChR7nDw/hqdefault.jpg",
-            url: "https://youtube.com/shorts/N_JtChR7nDw?si=vRnWgWkRxC18XYNp",
+            url: "https://www.youtube.com/embed/N_JtChR7nDw",
             title: "Video 4",
         },
         {
             id: 5,
             thumbnail: "https://img.youtube.com/vi/zT1gXMw05Qw/hqdefault.jpg",
-            url: "https://youtube.com/shorts/zT1gXMw05Qw?si=Y7E0px2g_YHO7Bm5",
+            url: "https://www.youtube.com/embed/zT1gXMw05Qw",
             title: "Video 5",
         },
     ];
@@ -57,7 +57,9 @@ const YouTube = () => {
                     onClick={() => openModal(video.url)}
                 >
                     <img src={video.thumbnail} alt={video.title} />
-                    <div className="play-button-overlay">▶</div>
+                    <div className="play-button-overlay">
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
                 </div>
                 ))}
             </div>
