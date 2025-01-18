25f8d017040747310584176fec385dd4eb22931a
dmswl2515 <dmswl2515@gmail.com>
2025-01-19 00:02:28 +0900

    인기콘텐츠 컴포넌트 틀 구현[skip ci]

---
+++ frontend/src/Component/PopularContent/PopularContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './PopularContent.css';
+
+const PopularContent = ({ contents }) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PopularContent;
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -3,8 +3,42 @@ import Carousel from '../Component/Swiper/Carousel';
 import CardIntroduction from '../Component/CardIntroduction/CardIntroduction';
 import Chart from '../Component/Chart/Chart';
 import YouTube from '../Component/YouTube/YouTube';
+import PopularContent from "../Component/PopularContent/PopularContent";
 
 function HomePage() {
+    const popularContents = [
+        {
+            image: "https://d1c5n4ri2guedi.cloudfront.net/post/3439/post_top_img/38006/card_1482_0.jpg", 
+            title: "2024 귀속 연말정산 준비 꿀팁 모음",
+            posts: 79,
+        },
+        {
+            image: "https://d1c5n4ri2guedi.cloudfront.net/display/4907/pc_img/38966/brand.jpg",
+            title: "모르면 손해! 브랜드별 할인 꿀팁 총집합",
+            posts: 144,
+        },
+        {
+            image: "https://d1c5n4ri2guedi.cloudfront.net/post/2966/post_top_img/33892/card_1401_0.jpg",
+            title: "출국 D-7, 해외여행 결제카드 준비!",
+            posts: 90,
+        },
+        {
+            image: "https://d1c5n4ri2guedi.cloudfront.net/post/2028/post_top_img/19598/card_1061_0.jpg",
+            title: "1 vs 1, 에디터가 깐깐하게 비교한 카드혜택",
+            posts: 59,
+        },
+        {
+            image: "https://d1c5n4ri2guedi.cloudfront.net/post/2480/post_top_img/28607/card_1247_0.jpg",
+            title: "월 200을 위한 알짜 신용·체크카드 추천",
+            posts: 75,
+        },
+        {
+            image: "https://d1c5n4ri2guedi.cloudfront.net/post/2585/post_top_img/29548/card_1283_0.jpg",
+            title: "NO 카드값 폭탄! 나만 안다고 했던 카드 꿀팁",
+            posts: 105,
+        },
+    ];
+
     return (
         <div>
             {/* Carousel Component */}
@@ -20,6 +54,9 @@ function HomePage() {
 
             {/* Chart Component */}
             <Chart />
+
+            {/* PopularContent Component */}
+            <PopularContent contents={popularContents} />
 
             {/* YouTube Component */}
             <YouTube />
