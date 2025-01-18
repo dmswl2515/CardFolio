633787f0b2792e18c4764b74ed856cc28317adf3
dmswl2515 <dmswl2515@gmail.com>
2025-01-19 01:00:10 +0900

    인기콘텐츠 css 수정 [skip ci]

--- frontend/src/Component/PopularContent/PopularContent.jsx
+++ frontend/src/Component/PopularContent/PopularContent.jsx
@@ -3,8 +3,22 @@ import './PopularContent.css';
 
 const PopularContent = ({ contents }) => {
     return (
-        <div>
-
+        <div className="popular-content">
+            <div className="section-header">
+                <h2>인기 콘텐츠</h2>
+                <button className="view-all-button">전체보기</button>
+            </div>
+            <div className="popular-content-container">
+                {contents.map((content, index) =>  (
+                    <div key={index} className="popular-card">
+                        <img src={content.image} alt={content.title} className="contents-image" />
+                        <div className="contents-info">
+                            <h3 className="contents-title">{content.title}</h3>
+                            <p className="contents-posts">{content.posts}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>    
         </div>
     );
 };
