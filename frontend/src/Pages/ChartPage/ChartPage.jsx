f44852678f2f2b6115df2645ee0a2fb3a508300c
dmswl2515 <dmswl2515@gmail.com>
2025-01-20 16:12:35 +0900

    카드폴리오 차트 섹션 구현 완료[skip ci]

--- frontend/src/Pages/ChartPage/ChartPage.jsx
+++ frontend/src/Pages/ChartPage/ChartPage.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ChartPage.css"
 import "../../Styles/Style.css";
 
 const ChartPage = () => {
+    const [currentTime, setCurrentTime ] = useState("");
+
+    useEffect(() => {
+        //current time
+        const updateClock = () => {
+            const now = new Date();
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            const seconds = String(now.getSeconds()).padStart(2, '0');
+            setCurrentTime(`${hours}:${minutes}:${seconds}`);
+        };
+
+        updateClock();
+        const interval = setInterval(updateClock, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return(
         <div className="chart-page">
             <div className="common-container">
@@ -11,20 +29,35 @@ const ChartPage = () => {
                     <h2>카드폴리오 차트</h2>
                     <div className="cardfolio-chart-container">
                         <div className="chart-items credit-card">
-                            <p className="realtime">실시간</p>
-                            <h3>신용카드 TOP100</h3>
+                            <p className="realtime">
+                                <i class="fa-regular fa-clock watch-icon"></i> 실시간 &nbsp;&nbsp;&nbsp;{currentTime}
+                            </p>
+                            <h3>
+                                <span class="title-small">신용카드</span> 
+                                <span class="title-large">TOP100</span>
+                            </h3>
                             <button className="btn credit-btn">보러가기</button>
                         </div>
                     
                         <div className="chart-items new-card">
-                            <p className="realtime">실시간</p>
-                            <h3>신용카드 TOP100</h3>
+                            <p className="realtime">
+                                <i class="fa-regular fa-clock watch-icon"></i> 실시간
+                            </p>
+                            <h3>
+                                <span class="title-small">신규카드</span> 
+                                <span class="title-large">TOP100</span>
+                            </h3>
                             <button className="btn new-btn">보러가기</button>
                         </div>
 
                         <div className="chart-items debit-card">
-                            <p className="realtime">실시간</p>
-                            <h3>체크카드 TOP100</h3>
+                            <p className="realtime">
+                                <i class="fa-regular fa-clock watch-icon"></i> 실시간
+                            </p>
+                            <h3>
+                                <span class="title-small">체크카드</span> 
+                                <span class="title-large">TOP100</span>
+                            </h3>
                             <button className="btn debit-btn">보러가기</button>
                         </div>
                     </div>
