68915329de9824a285cc26789827abc38ac5ead1
dmswl2515 <dmswl2515@gmail.com>
2025-01-20 17:23:54 +0900

    카드사별 인기차트 틀 구현[skip ci]

--- frontend/src/Pages/ChartPage/ChartPage.jsx
+++ frontend/src/Pages/ChartPage/ChartPage.jsx
@@ -3,10 +3,10 @@ import "./ChartPage.css"
 import "../../Styles/Style.css";
 
 const ChartPage = () => {
+    //current time
     const [currentTime, setCurrentTime ] = useState("");
 
     useEffect(() => {
-        //current time
         const updateClock = () => {
             const now = new Date();
             const hours = String(now.getHours()).padStart(2, '0');
@@ -20,6 +20,21 @@ const ChartPage = () => {
 
         return () => clearInterval(interval);
     }, []);
+
+    // card issuer date
+    const popularCards = [
+        { id: 1, logo: "신한카드", name: "신한카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/13/card_img/28201/13card.png"},
+        { id: 2, logo: "Samsung Card", name: "삼성카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/51/card_img/37691/51card.png" },
+        { id: 3, logo: "HyundaiCard", name: "현대카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/2669/card_img/32807/2669card.png" },
+        { id: 4, logo: "KB국민카드", name: "KB국민카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/2553/card_img/36834/2553card.png" },
+        { id: 5, logo: "우리카드", name: "우리카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/2441/card_img/37123/2441card_3.png" },
+        { id: 6, logo: "롯데카드", name: "롯데카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/2261/card_img/21011/2261card.png" },
+        { id: 7, logo: "하나카드", name: "하나카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/2657/card_img/32434/2657card.png" },
+        { id: 8, logo: "NH농협카드", name: "NH농협카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/666/card_img/21431/666card.png" },
+        { id: 9, logo: "BC 바로카드", name: "BC 바로카드", img: "https://d1c5n4ri2guedi.cloudfront.net/card/772/card_img/22246/772card.png" },
+        { id: 10, logo: "IBK기업은행", name: "IBK기업은행", img: "https://d1c5n4ri2guedi.cloudfront.net/card/2346/card_img/32523/2346card.png" },
+        
+    ];
 
     return(
         <div className="chart-page">
@@ -60,6 +75,22 @@ const ChartPage = () => {
                             </h3>
                             <button className="btn debit-btn">보러가기</button>
                         </div>
+                    </div>
+                </section>
+
+                {/* Card Issuer Ranking Section */}
+                <section className="popular-card-chart">
+                    <h2>카드사별 인기차트</h2>
+                    <div className="popular-card-container">
+                        {popularCards.map((card) => (
+                            <div key={card.id} className="card-box">
+                                <p className="card-logo">{card.logo}</p>
+                                <div className="card-content">
+                                    <h3 className="card-name">{card.name}</h3>
+                                    <img src={card.img} alt={card.name} className="card-image" />
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </section>
             </div>
