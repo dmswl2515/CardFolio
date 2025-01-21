b8b38249fd52634a8baf5efd54be228f20c48715
dmswl2515 <dmswl2515@gmail.com>
2025-01-21 10:36:30 +0900

    CardPage를 위한 jsx, css 파일 생성 및 app.js에 Route 추가[skip ci]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Layout from './Component/Layout';
 import HomePage from './Pages/HomePage';
 import ChartPage from './Pages/ChartPage/ChartPage';
+import CardPage from './Pages/CardPage/CardPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout><HomePage /></Layout>} />
       <Route path="/chart" element={<Layout><ChartPage /></Layout>}/>
+      <Route path="/card" element={<Layout><CardPage /></Layout>}/>
     </Routes>
   );
 }
---
+++ frontend/src/Pages/CardPage/CardPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "../../Styles/Style.css";
+import "./CardPage.css";
+
+export default CardPage;
