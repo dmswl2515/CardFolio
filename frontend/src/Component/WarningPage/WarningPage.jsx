c0d5c60fbc9a85357beb1dcddd8702cd084405bf
dmswl2515 <dmswl2515@gmail.com>
2025-01-23 14:12:38 +0900

    warningpage component css 적용[skip ci]

--- frontend/src/Component/WarningPage/WarningPage.jsx
+++ frontend/src/Component/WarningPage/WarningPage.jsx
@@ -3,8 +3,12 @@ import "./WarningPage.css";
 
 const WarningPage = ({ message }) => {
     return (
-        <div className="WarningPage">
-            {message || "현재 개발중인 페이지 입니다."}
+        <div className="page-background">
+            <div className="common-container">
+                <div className="WarningPage">
+                    {message || "현재 개발중인 페이지 입니다."}
+                </div>
+            </div>
         </div>
     );
 };
