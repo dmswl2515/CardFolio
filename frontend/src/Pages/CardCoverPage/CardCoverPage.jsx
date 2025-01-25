9dc6c8611c8d7d04306424d429c0913f42f75a12
dmswl2515 <dmswl2515@gmail.com>
2025-01-26 00:26:58 +0900

    today's best section 구현 완료[skip ci]

--- frontend/src/Pages/CardCoverPage/CardCoverPage.jsx
+++ frontend/src/Pages/CardCoverPage/CardCoverPage.jsx
@@ -3,6 +3,11 @@ import ContentData from "../../Component/ContentData";
 import "./CardCoverPage.css";
 
 const CardCoverPage = () => {
+    const topThreeContents = [...ContentData]
+        .filter(item => item.count !== undefined) 
+        .sort((a, b) => b.count - a.count) 
+        .slice(0, 3); 
+
     return (
         <div className="page-background">
             <div className="common-container">
@@ -26,12 +31,21 @@ const CardCoverPage = () => {
                             <p>인기캐릭터</p>
                             <h3>고스티 하이큐 정품 카드커버 출시! <br /> 유니폼 마킹 카드 만들기🏐</h3>
                             <button className="best-button">내용 확인하기 &nbsp;&nbsp;&nbsp; &gt; </button>
+                            <div className="best-images">
+                                <img 
+                                    src="https://d1c5n4ri2guedi.cloudfront.net/display/4960/pc_img/37965/2410_%EC%9D%B8%EC%8A%A4%ED%83%80%EA%B7%B8%EB%9E%A8_%ED%95%98%EC%9D%B4%ED%81%90_%EB%85%B8%ED%8A%B8%EB%B6%81_%EC%88%98%EC%A0%95.jpg"
+                                    alt="Best cover 1"
+                                />
+                            </div>
                         </div>
-                        <div className="best-images">
-                            <img 
-                                src="https://d1c5n4ri2guedi.cloudfront.net/display/4960/pc_img/37965/2410_%EC%9D%B8%EC%8A%A4%ED%83%80%EA%B7%B8%EB%9E%A8_%ED%95%98%EC%9D%B4%ED%81%90_%EB%85%B8%ED%8A%B8%EB%B6%81_%EC%88%98%EC%A0%95.jpg"
-                                alt="Best cover 1"
-                            />
+                        <div className="additional-info">
+                            {topThreeContents.map((item) => (
+                                <div key={item.id} className="top-content">
+                                    <img src={item.image} alt={item.name} className="top-content-image"/>
+                                    <p className="top-content-category">{item.category}</p>
+                                    <h4 className="top-content-name">{item.name}</h4>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </section>
