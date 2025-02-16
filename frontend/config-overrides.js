ca2a4d5797b65a6be62e96f59c824d32d99b9907
dmswl2515 <dmswl2515@gmail.com>
2025-02-16 22:29:47 +0900

    test for ci/cd

---
+++ frontend/config-overrides.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = function override(config, env) {
+  if (env === 'production') {
+    // output 경로 및 파일 이름 설정
+    config.output = {
+      ...config.output,
+      path: path.resolve(__dirname, 'build'),
+      filename: 'js/[name].[contenthash:8].js',
+      publicPath: '/',
+    };
+
+    // css 파일의 위치를 수정
+    const miniCssPlugin = config.plugins.find(
+      (plugin) => plugin.constructor.name === 'MiniCssExtractPlugin'
+    );
+
+    if (miniCssPlugin) {
+      miniCssPlugin.options.filename = 'css/[name].[contenthash:8].css';
+    }
+
+    // 정적 파일 경로 설정
+    config.module.rules.forEach((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf.forEach((one) => {
+          if (one.type === 'asset/resource') {
+            one.generator = {
+              filename: 'assets/[name].[contenthash:8][ext]',
+            };
+          }
+        });
+      }
+    });
+  }
+  return config;
+};
