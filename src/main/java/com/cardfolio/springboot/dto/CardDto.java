1bb890d35097f8f6b4d5b2f20df60a22ef453705
dmswl2515 <dmswl2515@gmail.com>
2025-02-22 23:07:23 +0900

    setting for jpa[skip ci]

---
+++ src/main/java/com/cardfolio/springboot/dto/CardDto.java
@@ -0,0 +1,49 @@
+package com.cardfolio.springboot.dto;
+
+import java.util.Date;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "CARDS")
+public class CardDto {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String img;
+	
+	private String name;
+	
+	private String type;
+	
+	private String event;
+	
+	private String company;
+	
+	private String benefit1;
+	
+	private String benefit2;
+	
+	private String benefit3;
+	
+	private String benefitcontent1;
+	
+	private String benefitcontent2;
+	
+	private String benefitcontent3;
+	
+	private String annualfee;
+	
+	private String condition;
+	
+	private String issueType;
+	
+	private int views;
+	
+	@Temporal(TemporalType.DATE)
+	private Date release;
+
+}
